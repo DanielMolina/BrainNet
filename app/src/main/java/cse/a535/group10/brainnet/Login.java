8216863d12d455c2ac366e0b312880261b2e53a7
DanielMolina <daniel.mol1082@gmail.com>
2018-12-06 20:50:18 -0700

    login without gif

---
+++ app/src/main/java/cse/a535/group10/brainnet/Login.java
@@ -0,0 +1,104 @@
+package cse.a535.group10.brainnet;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.support.annotation.NonNull;
+import android.support.v4.app.DialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
+
+import java.io.File;
+
+public class Login extends DialogFragment {
+    public static final int PICK_CSV = 0;
+
+    @Override
+    @NonNull
+    public Dialog onCreateDialog(final Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setMessage("Input Login ID");
+        builder.setCancelable(false);
+
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+        final View alertView = inflater.inflate(R.layout.dialog_signin, null);
+        final EditText edit = (EditText) alertView.findViewById(R.id.username); // return empty string
+
+        // Inflate and set the layout for the dialog
+        // Pass null as the parent view because its going in the dialog layout
+        builder.setView(alertView)
+                // Add action buttons
+                .setPositiveButton("Login", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int id) {
+                        String userID = edit.getText().toString();
+                        if (userID.equals("S007")) {
+                            // check phone for "recorded" brain waves
+                            File directory;
+                            String storagePath;
+
+                            // if no SD Card, check if data folder is already on phone
+                            if (Environment.getExternalStorageState() == null) {
+                                System.out.println("No SD Card Found");
+                                storagePath = Environment.getDataDirectory() + "/eegmmidb/";
+                                System.out.println(storagePath);
+                                directory = new File(storagePath);
+                                if (directory.exists()) {
+                                    Uri uri = Uri.parse(storagePath);
+                                    Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                                    intent.setDataAndType(uri, "*/*");
+                                    intent.putExtra("storage", storagePath);
+                                    getActivity().setIntent(intent);
+                                    getActivity().startActivityForResult(Intent.createChooser(intent, "Open"), PICK_CSV);
+                                } else
+                                    //System.out.println("Error");
+                                    dataAlert("EEG Data Folder Not Found");
+                            } else {
+                                System.out.println("SD Card Found");
+                                storagePath = Environment.getExternalStorageDirectory().getPath() + "/eegmmidb/";
+                                System.out.println(storagePath);
+                                directory = new File(storagePath);
+                                if (directory.exists()) {
+                                    Uri uri = Uri.parse(storagePath);
+                                    Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                                    intent.setDataAndType(uri, "*/*");
+                                    intent.putExtra("storage", storagePath);
+                                    getActivity().setIntent(intent);
+                                    getActivity().startActivityForResult(Intent.createChooser(intent, "Open"), PICK_CSV);
+                                } else
+                                    dataAlert("EEG Data Folder Not Found");
+                            }
+                        } else {
+                            dataAlert("Incorrect ID");
+                        }
+                    }
+                })
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+                    }
+                });
+
+        return builder.create();
+    }
+
+    /* method to create alert pop-up when data folder is not found */
+    protected void dataAlert(String msg) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setMessage(msg);
+        builder.setCancelable(true);
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                dialog.cancel();
+            }
+        });
+
+        AlertDialog alert = builder.create();
+        alert.show();
+    }
+}
--- app/src/main/java/cse/a535/group10/brainnet/MainActivity.java
+++ app/src/main/java/cse/a535/group10/brainnet/MainActivity.java
@@ -1,20 +1,18 @@
 package cse.a535.group10.brainnet;
 
 import android.Manifest;
-import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.pm.PackageManager;
-import android.net.Uri;
 import android.os.BatteryManager;
-import android.os.Environment;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+
 import android.view.View;
 import android.widget.Button;
 
@@ -24,28 +22,39 @@ import java.io.File;
 public class MainActivity extends AppCompatActivity {
     public static final int PICK_CSV = 0;
 
-    /* method to create alert pop-up when data folder is not found */
-    protected void dataAlert(String msg) {
-        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
-        builder.setMessage(msg);
-        builder.setCancelable(true);
-        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-            public void onClick(DialogInterface dialog, int id) {
-                dialog.cancel();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            // Permission is not granted, so request it
+            ActivityCompat.requestPermissions(MainActivity.this,
+                    new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                    1);
+        }
+
+        Button idButton = findViewById(R.id.idButton);
+        idButton.setOnClickListener(new View.OnClickListener() {
+            /* when clicked, the ID button on the main page will prompt the user to select EEG data
+             in .csv format from their SD card to test the model */
+            public void onClick(View v) {
+                // allow user to log in and show stimulus image for X seconds
+                login();
             }
         });
-
-        AlertDialog alert = builder.create();
-        alert.show();
     }
 
-    /* user selects csv to test and is sent to server */
+    protected void login() {
+        Login myDialog = new Login();
+        myDialog.show(getSupportFragmentManager(), "test");
+    }
+
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == PICK_CSV && resultCode == Activity.RESULT_OK) {
+        if (requestCode == PICK_CSV && resultCode == RESULT_OK) {
             if (data == null) {
-                System.out.println("Error Loading Data");
                 dataAlert("Error Loading Data");
             } else {
                 System.out.println("Data Selected");
@@ -82,60 +91,7 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
-                != PackageManager.PERMISSION_GRANTED) {
-            // Permission is not granted, so request it
-            ActivityCompat.requestPermissions(MainActivity.this,
-                    new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
-                    1);
-        }
-
-        final Button idButton = findViewById(R.id.idButton);
-        idButton.setOnClickListener(new View.OnClickListener() {
-            /* when clicked, the ID button on the main page will prompt the user to select EEG data
-             in .csv format from their SD card to test the model */
-            public void onClick(View v) {
-                File directory;
-                String storagePath;
-                // if no SD Card, check if data folder is already on phone
-                if (Environment.getExternalStorageState() == null) {
-                    System.out.println("No SD Card Found");
-                    storagePath = Environment.getDataDirectory() + "/eegmmidb/";
-                    System.out.println(storagePath);
-                    directory = new File(storagePath);
-                    if (directory.exists()) {
-                        Uri uri = Uri.parse(storagePath);
-                        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
-                        intent.setDataAndType(uri, "*/*");
-                        intent.putExtra("storage", storagePath);
-                        setIntent(intent);
-                        startActivityForResult(Intent.createChooser(intent, "Open"), PICK_CSV);
-                    } else
-                        dataAlert("EEG Data Folder Not Found");
-                } else {
-                    System.out.println("SD Card Found");
-                    storagePath = Environment.getExternalStorageDirectory().getPath() + "/eegmmidb/";
-                    System.out.println(storagePath);
-                    directory = new File(storagePath);
-                    if (directory.exists()) {
-                        Uri uri = Uri.parse(storagePath);
-                        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
-                        intent.setDataAndType(uri, "*/*");
-                        intent.putExtra("storage", storagePath);
-                        setIntent(intent);
-                        startActivityForResult(Intent.createChooser(intent, "Open"), PICK_CSV);
-                    } else
-                        dataAlert("EEG Data Folder Not Found");
-                }
-            }
-        });
-    }
-
+    /* get battery details */
     private void getBatteryStatus() {
         IntentFilter ifilter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
         Intent batteryStatus = this.registerReceiver(null, ifilter);
@@ -145,6 +101,21 @@ public class MainActivity extends AppCompatActivity {
             float batteryPct = level / (float) scale;
             Log.d("batter level", String.valueOf(batteryPct));
         }
+    }
+
+    /* method to create alert pop-up when data folder is not found */
+    protected void dataAlert(String msg) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage(msg);
+        builder.setCancelable(true);
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                dialog.cancel();
+            }
+        });
+
+        AlertDialog alert = builder.create();
+        alert.show();
     }
 }
 
